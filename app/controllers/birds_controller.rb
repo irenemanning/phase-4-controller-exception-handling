ac683e59488b23e64f6cd7ddd039773216dfcbb2
Irene <irenemanningm@gmail.com>
2023-07-09 17:48:49 -0500

    done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,7 @@
 class BirdsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
   # GET /birds
   def index
     birds = Bird.all
@@ -13,52 +15,66 @@ class BirdsController < ApplicationController
   end
 
   # GET /birds/:id
+  # def show
+  #   bird = find_bird
+  #   if bird
+  #     render json: bird
+  #   else
+  #     render_not_found_response
+  #   end
+  # end
   def show
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    bird = find_bird
+    render json: bird
   end
 
   # PATCH /birds/:id
+  # def update
+  #   bird = find_bird
+  #   if bird
+  #     bird.update(bird_params)
+  #     render json: bird
+  #   else
+  #     render_not_found_response
+  #   end
+  # end
   def update
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      bird.update(bird_params)
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    bird = find_bird
+    bird.update(bird_params)
+    render json: bird
   end
 
   # PATCH /birds/:id/like
   def increment_likes
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      bird.update(likes: bird.likes + 1)
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    bird = find_bird
+    bird.update(likes: bird.likes + 1)
+    render json: bird
   end
 
   # DELETE /birds/:id
   def destroy
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      bird.destroy
-      head :no_content
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    bird = find_bird
+    bird.destroy
+    head :no_content
   end
 
   private
 
   def bird_params
     params.permit(:name, :species, :likes)
+  end
+
+  # def find_bird
+  #   Bird.find_by(id: params[:id])
+  # end
+  # If we use the findLinks to an external site. method instead, we'll get an ActiveRecord::RecordNotFound exception instead of nil when the record doesn't exist. Try updating the find_bird action like this:
+  def find_bird
+    Bird.find(params[:id])
+  end
+  
+
+  def render_not_found_response
+    render json: { error: "Bird not found" }, status: :not_found
   end
 
 end
